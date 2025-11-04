b4cb46b32cbd86280c95c8bd7fdb8f5564ec1dd8
aikansh008 <aikanshtiwari007@gmail.com>
2025-11-05 03:30:17 +0530

    Initialize Go project with modules

---
+++ db/migrate/migrate.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"github.com/RakibSiddiquee/golang-gin-jwt-auth-crud/config"
+	"github.com/RakibSiddiquee/golang-gin-jwt-auth-crud/db/initializers"
+	"github.com/RakibSiddiquee/golang-gin-jwt-auth-crud/internal/models"
+	"log"
+)
+
+func init() {
+	config.LoadEnvVariables()
+	initializers.ConnectDB()
+}
+
+func main() {
+	err := initializers.DB.Migrator().DropTable(models.User{}, models.Category{}, models.Post{}, models.Comment{})
+	if err != nil {
+		log.Fatal("Table dropping failed")
+	}
+
+	err = initializers.DB.AutoMigrate(models.User{}, models.Category{}, models.Post{}, models.Comment{})
+
+	if err != nil {
+		log.Fatal("Migration failed")
+	}
+}
